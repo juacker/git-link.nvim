c6b457ea0c49af441b844769dcafd7378a556189
juan <2930882+juacker@users.noreply.github.com>
2023-12-12 00:37:29 +0100

    cleanup

--- lua/git-link/main.lua
+++ lua/git-link/main.lua
@@ -3,10 +3,8 @@ local function copy_to_clipboard(text)
 end
 
 local function open_url_in_browser(url)
-	-- Comando específico para abrir una URL en el navegador (puedes ajustarlo según tu sistema operativo)
-	local command = string.format("xdg-open %s", url) -- Comando para Linux (usando xdg-open)
+	local command = string.format("xdg-open %s", url) --Linux command (using xdg-open)
 
-	-- Ejecutar el comando en el sistema operativo
 	vim.fn.jobstart(command, {
 		detach = true,
 		cwd = vim.fn.getcwd(),
@@ -16,25 +14,19 @@ end
 local function get_remote_url()
 	local remote_url = vim.fn.trim(vim.fn.system("git config --get remote.origin.url"))
 
-	-- Si la URL es de tipo ssh, realiza la transformación basada en las definiciones de ~/.gitconfig
 	if remote_url:match("^ssh://") then
-		-- Extraer el dominio de la URL ssh
 		local domain = remote_url:match("ssh://([^/]+)")
 
-		-- Buscar definiciones en ~/.gitconfig para el dominio específico
 		local gitconfig_url = vim.fn.trim(vim.fn.system("git config --get-urlmatch url.insteadof ssh://" .. domain))
 
-		-- Si se encontró una definición en ~/.gitconfig, transformar la URL
 		if gitconfig_url ~= "" then
-			-- Extraer la parte del repositorio desde la raíz del dominio hasta .git
 			local repo_path = remote_url:match(domain .. "/(.+)%.git$")
 			if repo_path then
-				return gitconfig_url:gsub("%.git$", "") .. repo_path -- Construir la URL transformada
+				return gitconfig_url:gsub("%.git$", "") .. repo_path
 			end
 		end
 	end
 
-	-- Si no se realiza ninguna transformación, aplicar la conversión de URL según el patrón
 	local https_url =
 		remote_url:gsub("git@([^:]+):", "https://%1/"):gsub("ssh://git@([^:/]+)/", "https://%1/"):gsub("%.git$", "")
 
