dedc6d8685e1f49b9c3997bca515c5b7bb771864
juan <2930882+juacker@users.noreply.github.com>
2024-09-19 10:14:05 +0200

    cleanup

--- lua/git-link/main.lua
+++ lua/git-link/main.lua
@@ -3,12 +3,8 @@ local function copy_to_clipboard(text)
 end
 
 local function open_url_in_browser(url)
-	local command = string.format("xdg-open %s", url) --Linux command (using xdg-open)
-
-	vim.fn.jobstart(command, {
-		detach = true,
-		cwd = vim.fn.getcwd(),
-	})
+	local command = string.format("xdg-open %s", url) -- Linux command (using xdg-open)
+	vim.fn.jobstart(command, { detach = true, cwd = vim.fn.getcwd() })
 end
 
 local function get_remote_url()
@@ -16,7 +12,6 @@ local function get_remote_url()
 
 	if remote_url:match("^ssh://") then
 		local domain = remote_url:match("ssh://([^/]+)")
-
 		local gitconfig_url = vim.fn.trim(vim.fn.system("git config --get-urlmatch url.insteadof ssh://" .. domain))
 
 		if gitconfig_url ~= "" then
@@ -27,37 +22,27 @@ local function get_remote_url()
 		end
 	end
 
-	local https_url =
-		remote_url:gsub("git@([^:]+):", "https://%1/"):gsub("ssh://git@([^:/]+)/", "https://%1/"):gsub("%.git$", "")
-
-	return https_url
+	return remote_url:gsub("git@([^:]+):", "https://%1/"):gsub("ssh://git@([^:/]+)/", "https://%1/"):gsub("%.git$", "")
 end
 
 local function get_current_line_url()
 	local git_root = vim.fn.trim(vim.fn.system("git rev-parse --show-toplevel"))
 	local filename = vim.fn.expand("%:p"):gsub("^" .. git_root .. "/", "")
 	local linenr = vim.api.nvim_win_get_cursor(0)[1]
-
 	local relative_filename = vim.fn.trim(vim.fn.system("git ls-files --full-name " .. filename))
 	local remote_url = get_remote_url()
 
-	if remote_url then
-		return string.format("%s/blob/master/%s#L%d", remote_url, relative_filename, linenr)
-	end
+	return remote_url and string.format("%s/blob/master/%s#L%d", remote_url, relative_filename, linenr) or nil
 end
 
 local function copy_line_url()
 	local remote_url = get_current_line_url()
-	if remote_url then
-		copy_to_clipboard(remote_url)
-	end
+	if remote_url then copy_to_clipboard(remote_url) end
 end
 
 local function open_line_url()
 	local remote_url = get_current_line_url()
-	if remote_url then
-		open_url_in_browser(remote_url)
-	end
+	if remote_url then open_url_in_browser(remote_url) end
 end
 
 return {
--- plugin/git-link.vim
+++ plugin/git-link.vim
@@ -1,13 +1,6 @@
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>gu",
-  ':lua require("plugins.git-link.main").copy_line_url()<CR>',
-  { noremap = true, silent = true }
-)
+local function set_keymap(mode, lhs, rhs, opts)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+end
 
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>go",
-  ':lua require("plugins.git-link.main").open_line_url()<CR>',
-  { noremap = true, silent = true }
-)
+set_keymap("n", "<leader>gu", ':lua require("plugins.git-link.main").copy_line_url()<CR>', { noremap = true, silent = true })
+set_keymap("n", "<leader>go", ':lua require("plugins.git-link.main").open_line_url()<CR>', { noremap = true, silent = true })
